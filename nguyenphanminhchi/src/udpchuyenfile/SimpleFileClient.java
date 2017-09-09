71dc216a88b2bb03ef4e30423c6ab2a34bb5c4dc
ptud2017 <31211419+ptud2017@users.noreply.github.com>
2017-09-09 17:05:20 +0700

    Add files via upload

---
+++ nguyenphanminhchi/src/udpchuyenfile/SimpleFileClient.java
@@ -0,0 +1,57 @@
+package udpchuyenfile;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class SimpleFileClient {
+
+  public  static final int SOCKET_PORT = 80;      // you may change this
+  public  static final String SERVER = "127.0.0.1";  //localhost
+  public  static final String file_received = "T:/NGUYENPHANMINHCHI/tttwewe.txt";
+                                                            // different name because i don't want to
+                                                            // overwrite the one used by server...
+
+  public final static int FILE_SIZE = 6022386; // file size temporary hard coded
+                                               // should bigger than the file to be 
+
+  public static void main (String [] args ) throws IOException {
+    int bytesRead;
+    int current = 0;
+    FileOutputStream fos = null;
+    BufferedOutputStream bos = null;
+    Socket sock = null;
+    try {
+      sock = new Socket(SERVER, SOCKET_PORT);
+      System.out.println("Connecting...");
+
+      // receive file
+      byte [] mybytearray  = new byte [FILE_SIZE];
+      InputStream is = sock.getInputStream();
+      fos = new FileOutputStream(file_received);
+      bos = new BufferedOutputStream(fos);
+      bytesRead = is.read(mybytearray,0,mybytearray.length);
+      current = bytesRead;
+
+      do {
+         bytesRead =
+            is.read(mybytearray, current, (mybytearray.length-current));
+         if(bytesRead >= 0) current += bytesRead;
+      } while(bytesRead > -1);
+
+      bos.write(mybytearray, 0 , current);
+      bos.flush();
+      System.out.println("File " + file_received
+          + " downloaded (" + current + " bytes read)");
+    }
+    finally {
+      if (fos != null) fos.close();
+      if (bos != null) bos.close();
+      if (sock != null) sock.close();
+    }
+  }
+
+}
+
